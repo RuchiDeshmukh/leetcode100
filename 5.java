21d633cd8b119ea934c28da74e60fa3b75dde9b9
RuchiDeshmukh <57039206+RuchiDeshmukh@users.noreply.github.com>
2022-11-19 15:44:19 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/two-sum/
+class Solution {
+    //target = x+y
+    //y = target -x
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2];
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                output[0] = map.get(nums[i]);
+                output[1] = i;
+            }            
+
+            map.put(target-nums[i],i);
+        }
+        return output;
+    }
+}
