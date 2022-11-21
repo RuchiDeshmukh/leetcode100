60daba37d72af1cc43463fd131de92412ef963b4
RuchiDeshmukh <57039206+RuchiDeshmukh@users.noreply.github.com>
2022-11-21 21:01:47 +0530

    Create 10_alt.java

---
+++ 10_alt.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/valid-palindrome
+// takes less time for execution
+
+class Solution {
+    public boolean isPalindrome(String s) {
+    
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start < end){
+            while(start < end && !Character.isDigit(s.charAt(start)) &&
+             !Character.isLetter(s.charAt(start)) ) {
+                start++;
+            }
+
+            while(start < end && !Character.isDigit(s.charAt(end)) &&
+             !Character.isLetter(s.charAt(end)) ){
+                end--;
+            }
+
+            if(start >= end) break;
+
+            if(Character.toLowerCase(s.charAt(start)) != 
+            Character.toLowerCase(s.charAt(end))){
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
