d678414835753504b1bab1ac656e3d84b7a6e629
RuchiDeshmukh <57039206+RuchiDeshmukh@users.noreply.github.com>
2022-12-09 13:53:57 +0530

    Create 26.java

---
+++ 26.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/number-of-islands/
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid.length == 0 || grid[0].length == 0) return 0;
+        int count = 0;
+
+        for(int r = 0; r < grid.length; r++){
+            for(int c = 0; c < grid[0].length; c++){
+                if(grid[r][c] == '1'){
+                    count++;
+                    helper(grid,r,c);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void helper(char[][] grid, int r, int c){
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length 
+        || grid[r][c] =='0') return;
+        grid[r][c] = '0';
+        helper(grid,r+1,c);
+        helper(grid,r-1,c);
+        helper(grid,r,c-1);
+        helper(grid,r,c+1);
+    }
+}
